7c3f3587e8de815b219625d4c52d4d58e57bba30
Hacixanov Ehtiram <hajikhanoveh@KapMac223-M.local>
2026-03-30 15:28:38 +0400

    last updated

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("birinci ededi daxil edin");
+        int a = scanner.nextInt();
+        System.out.println("ikinci ededi daxil edin");
+        int b = scanner.nextInt();
+
         int result = 0;
-        for (int i = 0; i <=10 ; i++) {
+        for (int i = a; i <=b ; i++) {
 
             result +=i;
 
