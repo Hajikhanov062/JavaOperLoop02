c4d6d773f71438340a218af302cd69dafb1d8f97
Hacixanov Ehtiram <hajikhanoveh@KapMac223-M.local>
2026-03-30 12:57:10 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        int result = 0;
+        for (int i = 0; i <=10 ; i++) {
+            
+            result +=i;
+
+        }
+
+        System.out.println(result);
+    }
+}
